01d7c0f27e8488584df10a670890ef2d405ce1e8
NhanVo <nhanvt.hcm@gmail.com>
2024-09-29 09:50:42 +0000

    add html_generator

---
+++ dsl-jobs/html_generator.groovy
@@ -0,0 +1,23 @@
+pipelineJob('html-generator') {
+    parameters {
+        stringParam('DEPLOYED_BY',
+            defaultValue = '',
+            description = 'Identifies who generated the latest HTML.'
+        )
+    }
+    ...
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote { 
+                        url('https://github.com/min1907/go-html-renderer.git') // Your Github repo
+                        credentials("go-html-renderer") // Credentials ID from the previous step
+                    }
+                    branches('dynamic-html')
+                    scriptPath('Jenkinsfile')
+                }
+            }
+        }
+    }
+}
